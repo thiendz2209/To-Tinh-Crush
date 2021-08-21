6c09fa2e476f52118d6fb309f523f69e95aeab71
thiendz2209 <89286167+thiendz2209@users.noreply.github.com>
2021-08-21 23:38:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,21 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
+    introTitle: 'Duyên à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    Đằng sau chữ yêu đây là thương....Thời tiết trái gió trở trời 
+    trái tim lỡ nhịp cả đời thương em...
+    btnIntro: 'hihihi rớt nước mắt 🤧',
+    title: 'Em là người con gái anh yêu quý nhất từ trước đến giờ...
+    ',
+    desc: 'Nên em đừng đòi nợ anh nữa nhé bé 💁💁💁💁',
+    btnYes: 'Không đòi đâu anh em chỉ trêu ths🤍
+    btnNo: 'cứ đòi đấy anh 🤭zai
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại  cứ đòi nợ anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Thanks em gái nhiều nhé iu iu.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
